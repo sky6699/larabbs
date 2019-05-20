5952989e9a5c8b7c4b99f3476e5d5557a870f1b6
skyllxie <2480725237@qq.com>
2019-05-20 23:11:06 +0800

    邮箱认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class,      // <<--- 只需添加这一行
         ],
 
         'api' => [
@@ -42,7 +43,6 @@ class Kernel extends HttpKernel
             'bindings',
         ],
     ];
-
     /**
      * The application's route middleware.
      *
